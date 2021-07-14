bf86ac7de7d5ffc36d6a19eefeefe28f88106ee8
Edgars Vanags <e.vanags@accenture.com>
2021-07-14 17:06:49 +0300

    Add credits to author

--- assets/GifExtension.swift
+++ assets/GifExtension.swift
@@ -1,3 +1,11 @@
+//
+//  Gif.swift
+//  [SwiftGif](https://github.com/swiftgif/SwiftGif)
+//
+//  Created by Arne Bahlo on 07.06.14.
+//  Copyright (c) 2014 Arne Bahlo. All rights reserved.
+//
+
 import UIKit
 import ImageIO
 
